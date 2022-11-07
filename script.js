58080e0e7fcc884a50166d1f202dcd360b151ca9
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-11-06 19:04:03 -0500

    Include shells for important modules and factory functions

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const gameBoard = (() => {
   return {playMove};
 })();
 
+const displayController = (() => {
+  //const renderBoard = () => {};
+})();
+
+const gameState = (() => {
+  //const gameOver = false;
+})();
+
+const Player = (name, piece) => {
+  //gameState will decide which piece the player is when creating players
+  //const move = () => {};
+}
+
+const computerPlayer = (() => {
+  //minmax algorithm to create unbeatable AI
+})();
